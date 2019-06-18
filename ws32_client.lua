b1ccde2a10777e5958f935db6d5a85bd1f373b00
Alija Bobija <bobija.alija@gmail.com>
2019-06-18 09:30:10 +0200

    Refactoring
    Removed comments
    
    Spaces converted to tabs

--- ws32_client.lua
+++ ws32_client.lua
@@ -1,6 +1,7 @@
 M = {}
 
 local socket = nil
+local is_connected = false
 local on_connect_callback = nil
 local on_data_callback = nil
 
@@ -19,65 +20,58 @@ local buffer = ''
 
 local function decode_frame(frame)
     local data, fin
-  
-  if len_expected > 0 then
-    data = frame 
-    buffer = buffer .. data
-  else
-    -- FIN. 1 means msg is complete. 0 means multi-part
-    fin = frame:byte(1)
-    fin = bit.isset(fin, 7)
     
-    local opcode = frame:byte(1)
-    opcode = bit.clear(opcode, 4, 5, 6, 7)
-    print("Opcode:", opcode)
+    if len_expected > 0 then
+        data = frame 
+        buffer = buffer .. data
+    else
+        fin = frame:byte(1)
+        fin = bit.isset(fin, 7)
     
-    -- get 2nd byte as it has the payload length
-    -- msb of byte is mask, remaining 7 bytes is len 
-    local plen = frame:byte(2)
-    local mask = bit.isset(plen, 7)
+        local opcode = frame:byte(1)
+        opcode = bit.clear(opcode, 4, 5, 6, 7)
+        
+        print("Opcode:", opcode)
     
-    plen = bit.clear(plen, 7) -- remove the mask from the length
-    len_expected = plen
+        local plen = frame:byte(2)
+        local mask = bit.isset(plen, 7)
     
-    if mask then 
-      -- print("We should not get a mask from server. Error.")
-      return
+        plen = bit.clear(plen, 7)
+        len_expected = plen
+    
+        if mask then
+            return
+        end
+    
+        data = frame:sub(3)
+    
+        if plen == 126 then 
+            local extlen = frame:byte(3)
+            local extlen2 = frame:byte(4)
+        
+            extlen = bit.lshift(extlen, 8)
+        
+            len_expected = extlen + extlen2
+        
+            data = data:sub(3)
+        elseif plen == 127 then
+            data = data:sub(5)
+        end
+    
+        buffer = data
+    end 
+    
+    len_expected = len_expected - #data
+    
+    if len_expected <= 0 then
+        len_expected = 0
+    
+        if on_data_callback ~= nil then 
+            on_data_callback(buffer)
+        end
+    
+        buffer = ''
     end
-    
-    data = string.sub(frame, 3) -- remove first 2 bytes, i.e. start at 3rd char
-  
-    if plen == 126 then 
-      local extlen = frame:byte(3)
-      local extlen2 = frame:byte(4)
-      
-      extlen = bit.lshift(extlen, 8)
-      
-      len_expected = extlen + extlen2
-      
-      data = string.sub(data, 3) -- remove first 2 bytes
-      
-    elseif plen == 127 then 
-      -- print("Websocket lib does not support longer payloads yet")
-      -- return
-      data = string.sub(data, 5) -- remove first 4 bytes
-    end
-    
-    -- set the buffer to the current data since it's new
-    buffer = data
-  end 
-  
-  len_expected = len_expected - #data
-  
-  if len_expected <= 0 then
-    len_expected = 0
-    
-    if on_data_callback ~= nil then 
-      on_data_callback(buffer)
-    end
-    
-    buffer = ''
-  end
 end
 
 M.connect = function(ws_url)
@@ -124,6 +118,7 @@ M.connect = function(ws_url)
                 print('handshake done')
                 
                 if on_connect_callback ~= nil then
+                    is_connected = true
                     on_connect_callback(M)
                 end
             end
@@ -138,31 +133,25 @@ end
 M.send = function(data)
     print("send", data)
     
-    --if m.isConnected == false then 
-    --print("Websocket not connected, so cannot send.")
-    --return
-    --end 
-    
-    local binstr, payload_len 
+    if is_connected == false then 
+        print("Websocket not connected, so cannot send.")
+        return
+    end 
     
     if #data > 126 then 
         print("Lib only supports max len 126 currently")
         return
     end
+    
+    local binstr, payload_len 
 
-    -- set FIN to 1 meaning we will not multi-part this msg
-    binstr = string.char(bit.set(0x1, 7)) 
-    
-    -- 2nd byte mask and payload length
     payload_len = #data
-    payload_len = bit.set(payload_len, 7) -- set mask to on for 8th msb
-    binstr = binstr .. string.char(payload_len)
+    payload_len = bit.set(payload_len, 7)
     
-    -- 3rd, 4th, 5th, and 6th byte is masking key
-    -- just use mask of 0 to cheat so no need to xor
-    binstr = binstr .. string.char(0x0, 0x0, 0x0, 0x0)
-    
-    binstr = binstr .. data
+    binstr = string.char(bit.set(0x1, 7))
+        .. string.char(payload_len)
+        .. string.char(0x0, 0x0, 0x0, 0x0)
+        .. data
     
     socket:send(binstr)
 end
